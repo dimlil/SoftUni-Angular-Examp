18304cf952236a6cdbe82e9258cb7c47d63462dc
Dimitar Vassilev <d.v.vassilev@gmail.com>
2020-12-06 16:16:07 +0200

    feat/ added components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, HomeComponent, LoginComponent, RegisterComponent],
   imports: [
     CommonModule
   ],
