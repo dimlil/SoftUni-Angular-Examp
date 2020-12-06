b675f9df3358442ae28cb77bfe1d76829a80feca
Dimitar Vassilev <d.v.vassilev@gmail.com>
2020-12-06 16:41:19 +0200

    feat/ added header with logo

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { LogoComponent } from './logo/logo.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, HomeComponent, LoginComponent, RegisterComponent],
+  declarations: [HeaderComponent, HomeComponent, LoginComponent, RegisterComponent, LogoComponent],
   imports: [
     CommonModule
   ],
