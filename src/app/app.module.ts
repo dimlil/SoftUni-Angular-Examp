6ab445dab1d0e0ba965548c79b5dc415bf46f9b8
Dimitar Vassilev <d.v.vassilev@gmail.com>
2020-12-06 18:03:05 +0200

    feat/ created navigation between home,login and register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule,
     CoreModule
   ],
   providers: [],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,13 +5,15 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { LogoComponent } from './logo/logo.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [HeaderComponent, HomeComponent, LoginComponent, RegisterComponent, LogoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     HeaderComponent
