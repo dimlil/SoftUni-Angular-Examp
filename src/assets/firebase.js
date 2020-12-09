4b08cd5f7e1c00759251dfbd761c383cd8712579
Dimitar Vassilev <d.v.vassilev@gmail.com>
2020-12-09 17:35:17 +0200

    feat/ added firebase

---
+++ src/assets/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBuoyr9Pghf2YxIiI1r3JpqULQCYogL0k4",
+  authDomain: "softuni-angular-examp.firebaseapp.com",
+  projectId: "softuni-angular-examp",
+  storageBucket: "softuni-angular-examp.appspot.com",
+  messagingSenderId: "28417208246",
+  appId: "1:28417208246:web:3068692b00f3d32f341b17",
+  measurementId: "G-WX75MBX77B"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+const storage = firebaseApp.storage();
+
+export {
+  db,
+  auth,
+  storage
+}
